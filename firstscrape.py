41836a3c0e1dae4a8cba1d3cc999979321ed6d16
StoneColdSorcery <bradcf90@gmail>
2015-06-18 15:20:26 -0500

    added page iteration.. beginning incorporation of Critic and Review objects

--- firstscrape.py
+++ firstscrape.py
@@ -15,18 +15,31 @@ rvwTableClass = 'table-striped'
 
 
 
-
+'''
 
 
 class Review(object):
     
-    def __init__(self,tablerow):
-        try:
-            self.tverdict = tablerow.contents[0].span.get('class')
-        except Exception as E:
-            print('exception initializing review object: ',E)
-            self.tverdict = 'none'
-
+    def __init__(self,trow):
+        
+        if(len(trow) != 3):
+            raise ValueError
+        
+        self.verdictSpan = trow[0].find('span')
+        self.fOrR = self.verdictSpan.find('span').get('title')
+        self.tomatoScore = self.verdictSpan.find('span','tMeterScore').string
+        self.MovieTitle = trow[2].a.string
+        self.rvwLink = trow[3].a.get('href')
+    
+    
+    def printInfo(self):
+    
+        print("-" * 24)
+        print("Movie: ",self.MovieTitle)
+        print("Verdict: ",self.fOrR)
+        print("Tmeter Score: ",self.tomatoScore)
+        print("Review Link: ",self.rvwLink)
+    
 
 
 
@@ -37,64 +50,101 @@ class Critic(object):
         self.name = name
         self.hp_url = homepage
         self.reviews = []
-        r  = requests.get("http://" +url)
+        
+        r  = requests.get("http://" + homepage)
         data = r.text
         self.soup = BeautifulSoup(data)
         
-        for tbl in self.soup.find_all('table',rvwTableClass):
-            try:
-                print(len(tbl)," : ",tbl.get('class'), " : ",tbl.parent)
-                self.rvwTableBody = tbl.tbody
-            except Exception as E:
-                print('Exception in find table loop: ', E)
-                self.rvwTableBody = 0
-        #create review objects from rows of rvwTableBody
-        try:
-            self.reviews = [Review(r) for r in self.rvwTableBody.find_all('tr')]
-        except Exception as E:
-            print('Exception creating review objects: ', E)
-            
-            
-            
-    def printtverdicts(self):
-        for r in self.reviews:
-            print(r.tverdict)
+        #find td elements of class 'center'
+        #then look sideways to get the other 
+        # tds in the row
+        centds = self.soup.find_all('td','center') 
         
-    '''
+        parsedRows = [] #list for parsed row tags
+        
+        for td in centds:
+            print("href of center td: ",td.a.get('href'))
+            for nxt in td.findNext('td'):
+                print("contents of the findallnext: ",nxt.contents)     
+                mlink = td
+                rvwtxt = td.findNext('td')
+                tverdict = td.findPrevious('td')
+                tscore = tverdict.findPrevious('td')
+                parsedRows.append((tverdict,tscore,mlink,rvwtxt))
+                print(parsedRows[len(parsedRows) - 1])
+        
+        for r in parsedRows:
+            try:
+            
+                tempRowOject = Review(r)
+                self.reviews.append(Revi)
+            except Exception as E:
+                print('Exception creating review objects: ', E)
+            
+            
+            
+    def printVerdicts(self):
+        for r in self.reviews:
+            r.printInfo()
+        
+    
     def add_review(self,rvw):
         #LBYL, replace this with exception
         if (isinstance(rvw, Review)):
             self.reviews.append(rvw) #add review object to this critic
         else:
             print("Object: ",rvw," is not a Review Object")
-    '''
+
+
 
 
 url = "www.rottentomatoes.com/critic/david-rooney/"
 
 
-#critic1 = Critic('David',url)
+critic1 = Critic('David',url)
 
-#critic1.printtverdicts()
+critic1.printVerdicts()
 #look for 
 
-        
+
+'''
 
 #url = "www.rottentomatoes.com/critic/david-rooney/?cats=&genreid=&letter=&switches=&sortby=&limit=50&page=2"
-url = "www.rottentomatoes.com/critic/david-rooney/"
-r  = requests.get("http://" +url)
+baseurl = "www.rottentomatoes.com"
+pageurl = "/critic/david-rooney/"
+r  = requests.get("http://" +baseurl+ pageurl)
 
 data = r.text
 
 soup = BeautifulSoup(data)
 
-centds = soup.find_all('td','center')[30:36]
+nexttag = soup.find('a',text='Next')
+print(nexttag.get('href'))
+r  = requests.get("http://" +baseurl+ pageurl)
+try:
+    while(1):
+        
+        data = r.text
+        soup = BeautifulSoup(data)
+        nexttag = soup.find('a',text='Next')
+        nextpagelink = nexttag.get('href')
+        nextpageurl = "http://" +baseurl+ nextpagelink
+        print(nextpageurl)
+        r  = requests.get(nextpageurl)
+except Exception as e:
+    print("exception:",e)
+
+centds = soup.find_all('td','center')
+
+
+
+'''
 cmprows = []
 try:
     for td in centds:
         print("href of center td: ",td.a.get('href'))
-#         for nxt in td.findNext('td'):
-#             print("contents of the findallnext: ",nxt.contents)     
+        #for nxt in td.findNext('td'):
+            #print("contents of the findnext: ",nxt.contents)     
         mlink = td
         rvwtxt = td.findNext('td')
         tverdict = td.findPrevious('td')
@@ -104,11 +154,22 @@ try:
 except Exception as E:
     print("caught loopy error",E)
 
+
+nReviewsParsed = len(cmprows)
 for trow in cmprows:
     
     verdictSpan = trow[0].find('span')
-    fOrR = verdictSpan.find('span').get('title')
-    tomatoScore = verdictSpan.find('span','tMeterScore').string
+    try:
+        tomatoScore = verdictSpan.find('span','tMeterScore').string
+    except AttributeError:
+        tomatoScore = 'NA'    
+    
+    #fOrR = verdictSpan.find('span').get('title')
+    try:
+        fOrR = trow[1].find('span').get('class')[2] #3rd string in class attr is fresh/rotten
+    except AttributeError:
+        fOrR = 'VerdictError'   
+
     MovieTitle = trow[2].a.string
     rvwLink = trow[3].a.get('href')
     
@@ -117,8 +178,12 @@ for trow in cmprows:
     print("Verdict: ",fOrR)
     print("Tmeter Score: ",tomatoScore)
     print("Review Link: ",rvwLink)
+
+print("-" * 50)
+print("Parsed ",nReviewsParsed, "reviews")
 #print(soup.find(id="criticsReviewsChart_main"))
 # for link in soup.find(id="criticsReviewsChart_main")
 #     #print(link.get('href'))
 #     print(link)
     
+'''
